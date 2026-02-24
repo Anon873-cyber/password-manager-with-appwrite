4a9236d7068b3ed5078a00834218cd1b33a5f0f1
Anon873-cyber <async2304@gmail.com>
2026-02-24 16:13:00 +0530

    correct typo in variable name 'clint' to 'client

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,11 +1,13 @@
 import { Client, Account, OAuthProvider } from "appwrite";
 
 class Appwrite {
-    clint
+    clint;
+    account;
     constructor() {
         this.clint = new Client()
             .setEndpoint('https://<REGION>.cloud.appwrite.io/v1')
             .setProject('<PROJECT_ID>');
-
+            
+            this.account  = new Account(this.clint)
     }
 }
