48bcc240c3e9f0c2b802a6add5aeb4b68998d86c
Anon873-cyber <async2304@gmail.com>
2026-02-24 16:05:30 +0530

    boiler plate

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,7 +1,11 @@
 import { Client, Account, OAuthProvider } from "appwrite";
 
 class Appwrite {
+    clint
     constructor() {
-        
+        this.clint = new Client()
+            .setEndpoint('https://<REGION>.cloud.appwrite.io/v1')
+            .setProject('<PROJECT_ID>');
+
     }
 }
---
+++ src/conf/conf.js
@@ -0,0 +1,8 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectName: String(import.meta.env.VITE_APPWRITE_PROJECT_NAME),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+
+}
+
+export default conf
