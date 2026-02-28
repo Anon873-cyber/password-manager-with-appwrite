10445379497fefec7937bf862ecf0af85ac9998e
Anon873-cyber <async2304@gmail.com>
2026-02-28 13:08:05 +0530

    configured the button component so it can hold icon or svg

--- src/components/Ui/Button.jsx
+++ src/components/Ui/Button.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const Button = ({classname,type,children,...props}) => {
+const Button = ({
+  className = '',
+  type = 'button',
+  children,
+  icon = null,
+  iconPosition = 'left',
+  ...props
+}) => {
   return (
-   <button type={type} className={classname}{...props}>{children}</button>
+    <button
+      type={type}
+      className={`flex items-center justify-center gap-2 ${className}`}
+      {...props}
+    >
+      {icon && iconPosition === 'left' && icon}
+
+      {children}
+
+      {icon && iconPosition === 'right' && icon}
+    </button>
   )
 }
 
