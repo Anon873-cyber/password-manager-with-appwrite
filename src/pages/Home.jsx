96a0b5a6d9401d3c91a2aef97d1f4ad79f85b5b7
Anon873-cyber <async2304@gmail.com>
2026-03-14 16:17:00 +0530

    added css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,48 +22,52 @@ function Home() {
 
 
   return (
-  <div>
-    <PasswordForm />
+    <div className='mt-15'>
+      <PasswordForm />
 
-    <h2 className="text-xl font-bold mb-4 text-center">
-      Your Previous Passwords
-    </h2>
+      <h2 className="text-xl font-bold mb-4 text-center mt-8">
+        Your Previous Passwords
+      </h2>
 
-    {passwords.length === 0 ? (
-      <p className="text-center">No saved passwords</p>
-    ) : (
-      <table className="w-full table-fixed text-left">
-        <thead className="bg-gray-100">
-          <tr>
-            <th className="p-3">Site</th>
-            <th className="p-3">Username</th>
-            <th className="p-3">Password</th>
-            <th className="p-3">URL</th>
-          </tr>
-        </thead>
+      {passwords.length === 0 ? (
+        <p className="text-center ">No saved passwords</p>
+      ) : (
 
-        <tbody>
-          {passwords.map((item) => (
-            <tr key={item.$id} className="hover:bg-gray-50">
-              <td className="p-3 truncate">{item.siteName || "—"}</td>
-              <td className="p-3 truncate">{item.username}</td>
-              <td className="p-3 truncate">{item.password}</td>
-              <td className="p-3 truncate">
-                <a
-                  href={item.url}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="text-blue-500 underline"
-                >
-                  {item.url}
-                </a>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    )}
-  </div>
-);
+        <div className='h-[350px] overflow-auto'>
+
+          <table className="w-full table-fixed text-left">
+            <thead className="bg-[#016538] text-white">
+              <tr>
+                <th className="p-3">Site</th>
+                <th className="p-3">Username</th>
+                <th className="p-3">Password</th>
+                <th className="p-3">URL</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {passwords.map((item) => (
+                <tr key={item.$id} className="hover:bg-gray-50">
+                  <td className="p-3 truncate">{item.siteName || "No named"}</td>
+                  <td className="p-3 truncate">{item.username}</td>
+                  <td className="p-3 truncate">{item.password}</td>
+                  <td className="p-3 truncate">
+                    <a
+                      href={item.url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="text-blue-500 underline"
+                    >
+                      {item.url}
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
 }
 export default Home
